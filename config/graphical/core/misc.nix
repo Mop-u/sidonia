8002d334dcb2e084f065d7175503b1e68c729d9a
Quinn Unger <quinn.unger@pm.me>
2025-08-01 17:29:27 +0100

    remove v4l2loopback

--- config/graphical/core/misc.nix
+++ config/graphical/core/misc.nix
@@ -9,30 +9,23 @@ let
 in
 lib.mkIf (cfg.graphics.enable) {
 
-    # enable virtual camera for OBS
-    boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
-    boot.extraModprobeConfig = ''
-        options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    '';
-
     programs = {
         steam = {
             enable = true;
             protontricks.enable = true;
             extest.enable = true;
-            gamescopeSession =
-                {
-                    enable = true;
-                }
-                // (lib.mkIf config.hardware.nvidia.prime.offload.enable {
-                    env = {
-                        # for Prime render offload on Nvidia laptops.
-                        __NV_PRIME_RENDER_OFFLOAD = "1";
-                        __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA_G0";
-                        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-                        __VK_LAYER_NV_optimus = "NVIDIA_only";
-                    };
-                });
+            gamescopeSession = {
+                enable = true;
+            }
+            // (lib.mkIf config.hardware.nvidia.prime.offload.enable {
+                env = {
+                    # for Prime render offload on Nvidia laptops.
+                    __NV_PRIME_RENDER_OFFLOAD = "1";
+                    __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA_G0";
+                    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+                    __VK_LAYER_NV_optimus = "NVIDIA_only";
+                };
+            });
         };
         anime-games-launcher.enable = !cfg.graphics.legacyGpu; # multi launcher
     };
@@ -62,5 +55,10 @@ lib.mkIf (cfg.graphics.enable) {
                 ];
             };
         };
+        # enable virtual camera for OBS
+        #boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+        #boot.extraModprobeConfig = ''
+        #    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+        #'';
     };
 }
