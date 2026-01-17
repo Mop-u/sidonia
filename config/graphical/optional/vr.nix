1dc88c6a0576aa6ec8b07f046a26ed671023bfb7
Quinn Unger <quinn.unger@pm.me>
2026-01-17 20:16:38 +0000

    fixup vr option

--- config/graphical/optional/vr.nix
+++ config/graphical/optional/vr.nix
@@ -6,105 +6,56 @@
 }:
 let
     cfg = config.sidonia;
-    patchedBwrap = pkgs.bubblewrap.overrideAttrs (o: {
-        patches = (o.patches or [ ]) ++ [
-            (pkgs.writeTextFile {
-                name = "bwrap.patch";
-                text = ''
-                    diff --git a/bubblewrap.c b/bubblewrap.c
-                    index 8322ea0..4e20262 100644
-                    --- a/bubblewrap.c
-                    +++ b/bubblewrap.c
-                    @@ -868,13 +868,6 @@ acquire_privs (void)
-                           /* Keep only the required capabilities for setup */
-                           set_required_caps ();
-                         }
-                    -  else if (real_uid != 0 && has_caps ())
-                    -    {
-                    -      /* We have some capabilities in the non-setuid case, which should not happen.
-                    -         Probably caused by the binary being setcap instead of setuid which we
-                    -         don't support anymore */
-                    -      die ("Unexpected capabilities but not setuid, old file caps config?");
-                    -    }
-                       else if (real_uid == 0)
-                         {
-                           /* If our uid is 0, default to inheriting all caps; the caller
-                '';
-            })
-        ];
-    });
+
 in
 {
-    options.sidonia.services.vr = {
-        enable = lib.mkEnableOption "Enable VR services";
-        open.enable = lib.mkEnableOption "Enable SteamVR alternative services";
+    options.sidonia.services.vr.enable = lib.mkEnableOption "Enable VR services";
+    config = lib.mkIf (cfg.services.vr.enable) {
+        hardware.steam-hardware.enable = true;
+        programs.steam = {
+            package = pkgs.steam.override {
+                extraProfile = ''
+                    # Fixes timezones on VRChat
+                    unset TZ
+                    # Allows Monado to be used
+                    export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
+                '';
+            };
+            extraCompatPackages = [
+                pkgs.proton-ge-rtsp-bin
+            ];
+        };
+        services.monado = {
+            enable = true;
+            defaultRuntime = true;
+            highPriority = true;
+        };
+        systemd.user.services.monado.environment = {
+            STEAMVR_LH_ENABLE = "1";
+            XRT_COMPOSITOR_COMPUTE = "1";
+            IPC_EXIT_ON_DISCONNECT = "1";
+            WMR_HANDTRACKING = "0";
+        };
+        home-manager.users.${cfg.userName} = {
+            home.packages = [
+                pkgs.xrizer
+                pkgs.wlx-overlay-s
+            ];
+            xdg.configFile = {
+                "openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+                "openvr/openvrpaths.vrpath".text =
+                    let
+                        steamDir = "${config.home-manager.users.${cfg.userName}.xdg.dataHome}/Steam";
+                    in
+                    builtins.toJSON {
+                        version = 1;
+                        jsonid = "vrpathreg";
+                        external_drivers = null;
+                        config = [ "${steamDir}/config" ];
+                        log = [ "${steamDir}/logs" ];
+                        runtime = [ "${pkgs.xrizer}/lib/xrizer" ];
+                    };
+            };
+        };
     };
-    config = lib.mkIf (cfg.services.vr.enable) (
-        lib.mkMerge [
-            {
-                hardware.steam-hardware.enable = true;
-                programs.steam.extraCompatPackages = [
-                    pkgs.proton-ge-rtsp-bin
-                ];
-                home-manager.users.${cfg.userName} = {
-                    home.file = {
-                        ".local/share/Steam/ubuntu12_32/steam-runtime/usr/libexec/steam-runtime-tools-0/srt-bwrap" = {
-                            source = "${patchedBwrap}/bin/bwrap";
-                        };
-                    };
-                };
-                nixpkgs.overlays = [
-                    (final: prev: {
-                        steam = prev.steam.override {
-                            buildFHSEnv = (
-                                args:
-                                (
-                                    (pkgs.buildFHSEnv.override {
-                                        bubblewrap = patchedBwrap;
-                                    })
-                                    (args // { extraBwrapArgs = (args.extraBwrapArgs or [ ]) ++ [ "--cap-add ALL" ]; })
-                                )
-                            );
-                        };
-                    })
-                ];
-            }
-            (lib.mkIf (cfg.services.vr.open.enable) {
-                services.monado = {
-                    enable = true;
-                    defaultRuntime = true;
-                };
-                systemd.user.services.monado.environment = {
-                    STEAMVR_LH_ENABLE = "1";
-                    XRT_COMPOSITOR_COMPUTE = "1";
-                };
-                home-manager.users.${cfg.userName} = {
-                    home.packages = [
-                        pkgs.xrizer
-                        pkgs.wlx-overlay-s
-                    ];
-                    xdg.configFile = {
-                        "openxr/1/active_runtime.json".source =
-                            "${config.services.monado.package}/share/openxr/1/openxr_monado.json";
-
-                        "openvr/openvrpaths.vrpath".text = builtins.toJSON {
-                            config = [ "${config.home-manager.users.${cfg.userName}.xdg.dataHome}/Steam/config" ];
-                            external_drivers = null;
-                            jsonid = "vrpathreg";
-                            log = [ "${config.home-manager.users.${cfg.userName}.xdg.dataHome}/Steam/logs" ];
-                            runtime = [ "${pkgs.xrizer}/lib/xrizer" ];
-                            version = 1;
-                        };
-                    };
-                    home.file = {
-                        ".local/share/monado/hand-tracking-models".source = pkgs.fetchgit {
-                            url = "https://gitlab.freedesktop.org/monado/utilities/hand-tracking-models";
-                            sha256 = "sha256-x/X4HyyHdQUxn3CdMbWj5cfLvV7UyQe1D01H93UCk+M=";
-                            fetchLFS = true;
-                        };
-                    };
-                };
-            })
-        ]
-    );
 }
