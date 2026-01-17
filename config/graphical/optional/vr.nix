0bbc40a45313b2c14b480dce1015a4893d34ebc0
Quinn Unger <quinn.unger@pm.me>
2026-01-17 23:08:20 +0000

    drop wlx-overlay-s

--- config/graphical/optional/vr.nix
+++ config/graphical/optional/vr.nix
@@ -39,7 +39,6 @@ in
         home-manager.users.${cfg.userName} = {
             home.packages = [
                 pkgs.xrizer
-                pkgs.wlx-overlay-s
             ];
             xdg.configFile = {
                 "openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
