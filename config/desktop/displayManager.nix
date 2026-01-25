d55320513b1b7c7f8be304498042b0171abd9225
Quinn Unger <quinn.unger@pm.me>
2026-01-25 23:57:48 +0000

    fix hyprland warning

--- config/desktop/displayManager.nix
+++ config/desktop/displayManager.nix
@@ -18,7 +18,28 @@ in
             };
         })
         (lib.mkIf (cfg.programs.hyprland.enable) {
-            services.displayManager.defaultSession = lib.mkDefault "hyprland-uwsm";
+            services.displayManager = {
+                # https://github.com/hyprwm/Hyprland/discussions/12661
+                sessionPackages = [
+                    (pkgs.writeTextFile {
+                        name = "hyprland-uwsm-fixed";
+                        text = ''
+                            [Desktop Entry]
+                            Name=Hyprland (UWSM)
+                            Comment=Hyprland compositor managed by UWSM
+                            Exec=${lib.getExe config.programs.uwsm.package} start -F -- /run/current-system/sw/bin/start-hyprland
+                            Type=Application
+                            DesktopNames=Hyprland
+                            Keywords=tiling;wayland;compositor;
+                        '';
+                        destination = "/share/wayland-sessions/hyprland-uwsm-fixed.desktop";
+                        derivationArgs = {
+                            passthru.providedSessions = [ "hyprland-uwsm-fixed" ];
+                        };
+                    })
+                ];
+                defaultSession = lib.mkDefault "hyprland-uwsm-fixed";
+            };
         })
     ];
 }
