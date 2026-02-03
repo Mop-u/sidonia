3d24d12cdd59f4fd452bc6003ed57603845c3f18
Quinn Unger <quinn.unger@pm.me>
2026-02-03 19:41:14 +0000

    fix overlays

--- overlays.nix
+++ overlays.nix
@@ -53,5 +53,5 @@ in
     })
     (overlayMissingFromFlake inputs.nixpkgs-xr) # use nixpkgs stable where possible
     inputs.niri.overlays.niri
-    inputs.moppkgs.overlays
+    inputs.moppkgs.overlays.default
 ]
