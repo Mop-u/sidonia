1ab04b7abb1b934fb86f35bd043285ddf3dffb21
Quinn Unger <quinn.unger@pm.me>
2026-02-23 14:18:47 +0000

    nemo: fix gsettings / dconf schemas

--- config/hm/programs/nemo.nix
+++ config/hm/programs/nemo.nix
@@ -7,31 +7,39 @@
 }:
 let
     cfg = osConfig.sidonia;
+    nemoBasePackage = pkgs.nemo;
+    nemoPackage =
+        (pkgs.nemo-with-extensions.overrideAttrs { extraNativeBuildInputs = [ pkgs.gvfs ]; }).override
+            {
+                nemo = nemoBasePackage;
+                extensions = with pkgs; [
+                    folder-color-switcher
+                    nemo-emblems
+                    nemo-fileroller
+                    nemo-python
+                    nemo-preview
+                ];
+                useDefaultExtensions = false;
+            };
 in
 lib.mkIf (cfg.desktop.enable) {
     home.packages = with pkgs; [
         mate.engrampa # archive manager
         ffmpegthumbnailer
         webp-pixbuf-loader
-        cinnamon-gsettings-overrides
-        ((nemo-with-extensions.overrideAttrs { extraNativeBuildInputs = [ pkgs.gvfs ]; }).override {
-            extensions = [
-                folder-color-switcher
-                nemo-emblems
-                nemo-fileroller
-                nemo-python
-                nemo-preview
-                nemo-seahorse
-            ];
-            useDefaultExtensions = false;
-        })
+        nemoPackage
     ];
-    xdg.mimeApps.defaultApplications =
-        let
-            fileExplorer = "nemo.desktop";
-        in
-        {
-            "inode/directory" = [ fileExplorer ];
-            "application/x-gnome-saved-search" = [ fileExplorer ];
-        };
+    xdg = {
+        systemDirs.data = [
+            "${nemoPackage}/share/gsettings-schemas/nemo-${nemoBasePackage.version}"
+        ];
+        mimeApps.defaultApplications =
+            let
+                fileExplorer = "nemo.desktop";
+            in
+            {
+                "inode/directory" = [ fileExplorer ];
+                "application/x-gnome-saved-search" = [ fileExplorer ];
+            };
+    };
 }
