fa16895e161e03b3264e95bea785db3b6e6c5d08
Quinn Unger <quinn.unger@pm.me>
2026-03-07 16:14:00 +0000

    fix recursion in kdeconnect plugin setup

--- config/hm/shell/noctalia/settings/plugins/kde-connect.nix
+++ config/hm/shell/noctalia/settings/plugins/kde-connect.nix
@@ -8,13 +8,16 @@
 let
     cfg = osConfig.sidonia;
     sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
-    enableKdeConnect = osConfig.programs.kdeconnect.enable || config.services.kdeconnect.enable;
 in
-lib.mkIf (cfg.desktop.enable && (cfg.desktop.shell == "noctalia") && enableKdeConnect) {
-    services.kdeconnect.enable = lib.mkDefault osConfig.programs.kdeconnect.enable;
-    home.packages = [ pkgs.kdePackages.qttools ];
-    programs.noctalia-shell.plugins.states.kde-connect = {
-        enabled = lib.mkDefault true;
-        inherit sourceUrl;
-    };
-}
+lib.mkIf (cfg.desktop.enable && (cfg.desktop.shell == "noctalia")) (
+    lib.mkMerge [
+        { services.kdeconnect.enable = lib.mkDefault osConfig.programs.kdeconnect.enable; }
+        (lib.mkIf osConfig.programs.kdeconnect.enable {
+            home.packages = [ pkgs.kdePackages.qttools ];
+            programs.noctalia-shell.plugins.states.kde-connect = {
+                enabled = lib.mkDefault true;
+                inherit sourceUrl;
+            };
+        })
+    ]
+)
