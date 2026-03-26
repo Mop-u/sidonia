5027233b5ab8d399acc18b6c088f464668c06e99
Quinn Unger <quinn.unger@pm.me>
2026-03-26 14:35:49 +0000

    noctalia-shell: add screen-recorder plugin defaults

--- config/hm/shell/noctalia/settings/plugins/screen-recorder.nix
+++ config/hm/shell/noctalia/settings/plugins/screen-recorder.nix
@@ -17,7 +17,28 @@ lib.mkIf (cfg.desktop.enable && (cfg.desktop.shell == "noctalia")) {
             enabled = lib.mkDefault enable;
             inherit sourceUrl;
         };
-        #pluginSettings.screen-recorder = {};
+        pluginSettings.screen-recorder = {
+            hideInactive = false;
+            iconColor = "none";
+            directory = "${config.home.homeDirectory}/Videos";
+            filenamePattern = "recording_yyyyMMdd_HHmmss";
+            frameRate = "custom";
+            customFrameRate = "60";
+            audioCodec = "opus";
+            videoCodec = "h264";
+            quality = "very_high";
+            colorRange = "limited";
+            showCursor = true;
+            copyToClipboard = false;
+            audioSource = "default_output";
+            videoSource = "portal";
+            resolution = "original";
+            restorePortalSession = false;
+            replayEnabled = false;
+            replayDuration = "custom";
+            customReplayDuration = "30";
+            replayStorage = "ram";
+        };
     };
     home.packages = lib.optional enable osConfig.programs.gpu-screen-recorder.package;
 }
