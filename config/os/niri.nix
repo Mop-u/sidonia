ad7ec7b64c590d11707316d7ded1b13affd376b7
Quinn Unger <quinn.unger@pm.me>
2026-06-13 17:34:37 +0100

    default to nixpkgs niri (it has blur support now)

--- config/os/niri.nix
+++ config/os/niri.nix
@@ -10,7 +10,7 @@ in
 lib.mkIf (cfg.desktop.enable && (cfg.desktop.compositor == "niri")) {
   programs.niri = {
     enable = true;
-    package = lib.mkDefault (if cfg.graphics.legacyGpu then pkgs.niri-legacy else pkgs.niri-unstable);
+    package = lib.mkDefault (if cfg.graphics.legacyGpu then pkgs.niri-legacy else pkgs.niri);
     useNautilus = lib.mkDefault false;
     withUWSM = lib.mkDefault true;
     withXDG = lib.mkDefault true;
